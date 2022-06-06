38d00a37c2df58e0e183fff0414eb635654828d7
Alexandre Vieira <kitodann@gmail.com>
2022-06-06 19:26:30 -0300

    Created the questions controller and added resources to it

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,7 @@
+class QuestionsController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+end
---
+++ test/controllers/questions_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class QuestionsControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get questions_show_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get questions_create_url
+    assert_response :success
+  end
+end
