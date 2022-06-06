8f985f10ad019a9c76aee65d73639f2e2b8c3975
Alexandre Vieira <kitodann@gmail.com>
2022-06-06 20:07:44 -0300

    Added the lessons controller changed the routes nesting answers 'create' inside the questions resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  
   resources :users, only: [:show]
-  resources :questions, only: [:show, :create]
-  resources :answers, only: [:create, :update]
-  
+
+  resources :questions, only: %i[show create] do
+    resources :answers, only: %i[create]
+  end
+  resources :answers, only: %i[update]
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
