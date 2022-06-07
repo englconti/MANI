e67d6ed6dfb184516ddc04db86b52f456eff4d4a
Alexandre Vieira <kitodann@gmail.com>
2022-06-07 18:07:08 -0300

    Edited the relations inside the lessons model, created the show question, began the styling of the questions view show

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :lesson
+
+  validates :question_type, presence: true, inclusion: { in: %w[abcd writing link] }
+  validates :statement, presence: true
+  validates :correct_answer, presence: true
 end
