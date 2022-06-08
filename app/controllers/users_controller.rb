ae6f44f2318dd11c9b820abe65ae2df1273904e0
Leonardo Conti <eng.leonardoconti@gmail.com>
2022-06-08 00:25:57 -0300

    retrieving-answer-data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
   def show
     @user = current_user
 
+    l1 = Lesson.find(1)
+    @l1_num_questions = l1.questions.count
+    l1_answers = @user.answers.where("question_id < ?", l1.questions.count + 1)
+    @l1_correct_answers = l1_answers.where("is_correct = ?", true).count
+
+
     # Do the logic to get the answers from the user in all lessons and render it in the show page.
     # ON PROGRESS
     # Lesson.all.each do |lesson|
