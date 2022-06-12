d7bb43fc9a25b1c387a51ac09285cb3a31dd324f
Leonardo Conti <eng.leonardoconti@gmail.com>
2022-06-12 01:09:20 -0300

    questions-page-upgraded-&-bar-logic

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,25 +4,7 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
-
     @answer_id = Answer.where(question: @question, user: current_user)[0].id
-
-    i = 0
-    if [1, 6, 11, 16, 21].include? @question.id
-      i = 0
-    elsif [2, 7, 12, 17, 22].include? @question.id
-      i = 1
-    elsif [3, 8, 13, 18, 23].include? @question.id
-      i = 2
-    elsif [4, 9, 14, 19, 24].include? @question.id
-      i = 3
-    elsif [5, 10, 15, 20, 25].include? @question.id
-      i = 4
-    end
-
-    array_of_advancement = ["0", "12vw", "24vw", "36vw", "48vw", "60vw"]
-
-    @bar_advancement = array_of_advancement[i]
   end
 
   # Questions are created on the seed file!
--- app/javascript/controllers/next_question_controller.js
+++ app/javascript/controllers/next_question_controller.js
@@ -6,16 +6,20 @@ export default class extends Controller {
 
   connect() {
     console.log("next_question connected")
+    // 0. Feature to make the bar move -----
+    this.barStatusLoad(0)
+    // -------------------------------------
   }
 
   confirm(event) {
-    event.preventDefault()
-    this.element.setAttribute("hidden", true)
+    event.preventDefault();
+
+    this.element.setAttribute("hidden", true);
 
     this.questionElement = document.querySelector("#questions-container");
 
     // 1. Getting the text content from the marked Card and puttinuserAnswer
-    const userAnswer = document.querySelector(".marked").children[0].children[1].textContent
+    const userAnswer = document.querySelector(".marked").children[1].textContent;
     // console.log(userAnswer)
 
     // 2. Defining the correct URL based on the server routes. OBS: it will need to be change for Heroku deployment
@@ -25,7 +29,7 @@ export default class extends Controller {
     // const urlPatch = `https://mani-finance.herokuapp.com/answers/${this.questionElement.dataset.answerId}`;
 
     // 3. Creating a JS Object with the data we want to send to the server
-    const data = { answer: userAnswer, answer_id: this.questionElement.dataset.answerId }
+    const data = { answer: userAnswer, answer_id: this.questionElement.dataset.answerId };
 
     // 4. Send data to correct route - In this case PATCH, since we will only be updating answers.
     fetch(urlPatch, {
@@ -46,23 +50,34 @@ export default class extends Controller {
     const questionCards = document.querySelectorAll(".question-card");
 
     questionCards.forEach((questionCard) => {
-      // console.log(questionCard.classList.contains("marked"));
+      questionCard.style.pointerEvents = 'none';
       if (!questionCard.classList.contains("marked")) {
-        questionCard.classList.add("disabled-question-card");
-        questionCard.classList.remove("question-card");
-        questionCard.classList.remove(questionCard.classList[0]);
-      } else {
-        questionCard.classList.remove(questionCard.classList[1]);
+        questionCard.classList.add("question-card-greyed");
       }
     })
 
     const correctAnswer = document.querySelector("#correct-answer").textContent
+
     if (userAnswer === correctAnswer) {
       document.querySelector('.correct-answer').removeAttribute("hidden");
       document.querySelector('.marked').classList.add("correct-answer-card");
     } else {
+      document.querySelector('.marked').classList.add("wrong-answer-card");
       document.querySelector('.wrong-answer').removeAttribute("hidden");
     }
 
+      // 7. Feature to make the bar move -----
+      this.barStatusLoad(1)
+      // -------------------------------------
+  }
+
+  barStatusLoad = function (num) {
+      const questionElement = document.querySelector("#questions-container");
+      const currentQuestionId = questionElement.dataset.questionId;
+      const questionsCount = questionElement.dataset.lessonQuestionsCount;
+      const firstQuestionId = questionElement.dataset.firstQuestionId;
+      const progress = (currentQuestionId - firstQuestionId + num) * (60 / questionsCount);
+      const progressBar = document.querySelector(".bar-container");
+      progressBar.style = `width: ${progress}vw;`
   }
 }
