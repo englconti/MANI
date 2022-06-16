81949a29de86ea2572f6adfe9aae2c530d983332
Alexandre Vieira <kitodann@gmail.com>
2022-06-15 22:25:53 -0300

    created the writing questions page, changed the questions controller created the writing questions js controller with a method to select each option from the user, created a partil of the abcd and the writing questions. To-do: move the footer also into the partials because each of them will need a different confirm button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def remove_words_from_statement(statement, question)
+    remove_text = statement.gsub(question.a, "<div data-wr-choice-target='choice'>_______</div>") if question.a.present?
+    remove_text = remove_text.gsub(question.b, "<div data-wr-choice-target='choice'>_______</div>") if question.b.present?
+    remove_text = remove_text.gsub(question.c, "<div data-wr-choice-target='choice'>_______</div>") if question.c.present?
+    remove_text = remove_text.gsub(question.d, "<div data-wr-choice-target='choice'>_______</div>") if question.d.present?
+    remove_text
+  end
 end
